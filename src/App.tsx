aaa7ab091b5b9a4a0663d70f8e31e8568ccc4adb
Raphael <naduma.raphael4@gmail.com>
2025-01-27 19:56:07 +0800

    CHECKPOINT

--- src/App.tsx
+++ src/App.tsx
@@ -8,27 +8,9 @@ const App = () => {
   return (
     // bg-linear-to-bl from-indigo-950 via-gray-900 to-black
 
-    <div className="absolute inset-0 p-4 w-full h-full bg-cover bg-no-repeat bg-fixed bg-center flex flex-col justify-center items-center bg-[url('https://usagif.com/wp-content/uploads/gifs/starfall-gif-46.gif')]">
-      {/* SIZE INDICATORS */}
-      <ul>
-        <li className="font-bold uppercase text-2xl inline sm:hidden md:hidden lg:hidden xl:hidden">
-          xs
-        </li>
-        <li className="font-bold uppercase text-2xl hidden sm:inline md:hidden lg:hidden xl:hidden">
-          sm
-        </li>
-        <li className="font-bold uppercase text-2xl hidden sm:hidden md:inline lg:hidden xl:hidden">
-          md
-        </li>
-        <li className="font-bold uppercase text-2xl hidden sm:hidden md:hidden lg:inline xl:hidden">
-          lg
-        </li>
-        <li className="font-bold uppercase text-2xl hidden sm:hidden md:hidden lg:hidden xl:inline">
-          xl
-        </li>
-      </ul>
+    <div className="absolute space-y-8 inset-0 p-4 w-full h-full bg-cover bg-no-repeat bg-fixed bg-center flex flex-col justify-center items-center bg-[url('https://usagif.com/wp-content/uploads/gifs/starfall-gif-46.gif')]">
 
-      <h1 className="font-serif font-bold text-white text-3xl">To-Do List</h1>
+      <h1 className="font-serif font-bold text-white text-5xl mt-auto">To-Do List</h1>
 
       <TodoContainer>
         <TodoList></TodoList>
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -8,13 +8,14 @@ import { CiCircleChevDown } from "react-icons/ci";
 
 const TodoList = () => {
   const [tasks, setTasks] = useState([
-    "task 1",
-    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, ratione!",
-    "Lorem ipsum,dolorsitametconsecteturadipisicingelit.Placeat,ratione!",
-    "task 2",
-    "task 3",
-    "DESIGN START",
-    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, ratione!",
+    "Hover then",
+    "[Shift + scroll]",
+    "to scroll",
+    "horizontally.",
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam consequatur est ipsum officiis eveniet voluptates, amet magnam, obcaecati eligendi maiores laborum recusandae, illo animi a ipsam suscipit eos molestiae tempore rerum in corporis.",
+    "Accusantium qui facilis nisi doloribus sapiente beatae saepe consequatur magni sequi quidem voluptatem quaerat unde voluptatibus corporis recusandae laudantium, quas amet cum blanditiis, animi molestiae.",
+    "Ipsa possimus culpa, ducimus nemo dicta debitis pariatur explicabo quisquam molestiae non placeat laboriosam iusto voluptates aut error quod recusandae animi beatae incidunt quo dolorem ratione totam cumque.",
+    "Ipsa totam fugiat ut saepe quia, illo assumenda nostrum, vero eius dolor esse nam?"
   ]);
   const [newTask, setNewTask] = useState("");
 
@@ -62,7 +63,7 @@ const TodoList = () => {
     <div className="w-full">
       <div className="top-0 w-full grid grid-cols-[1fr_25%] mb-8">
         <input
-          className="bg-white rounded-tl-md rounded-bl-md px-4 text-black"
+          className="bg-transparent text-xl backdrop-blur-sm rounded-tl-md rounded-bl-md px-4 text-black active:scale-95 transition-all duration-100 font-mono"
           type="text"
           placeholder="Enter task"
           value={newTask}
@@ -70,7 +71,7 @@ const TodoList = () => {
           onChange={handleInput}
         />
         <Button
-          className="bg-green-400 hover:bg-green-600 text-black hover:text-white cursor-pointer rounded-tl-none rounded-bl-none rounded-tr-md rounded-br-md"
+          className="bg-green-500/50 backdrop-blur-sm hover:bg-green-600/60 text-black text-xl hover:scale-105 hover:text-white cursor-pointer rounded-tl-none rounded-bl-none rounded-tr-md rounded-br-md transition-all duration-100"
           onClick={addTask}
         >
           Add
@@ -82,11 +83,13 @@ const TodoList = () => {
           {tasks.map((task, index) => (
             <li
               key={index}
-              className="w-full h-1/5 backdrop-blur-xs border-2 border-lime-400 rounded-2xl p-2 grid items-center grid-cols-[10%_1fr_64px]"
+              className="w-full h-1/5 backdrop-blur-xs border-2 border-lime-400 rounded-2xl p-2 grid items-center grid-cols-[10%_1fr_64px] text-nowrap"
             >
               <Checkbox className="cursor-pointer hover:bg-black/10 active:bg-white" />
 
-              <span className="flex overflow-scroll custom-scrollbar text-black w-9/10">{task}</span>
+              <span className="flex overflow-scroll custom-scrollbar text-black text-2xl font-bold font-mono w-9/10">
+                {task}
+              </span>
 
               <span className="grid grid-cols-[1fr_1fr]">
                 <MdDeleteOutline
