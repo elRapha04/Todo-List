83bf5ab8e51bc5eebec1bf2743482007597c77be
Raphael <naduma.raphael4@gmail.com>
2025-01-27 09:35:04 +0800

    install shadcn components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="absolute inset-0 p-4 w-full h-full bg-cover bg-no-repeat flex flex-col justify-center items-center bg-gradient-to-bl from-indigo-950 via-gray-900 to-black">
+      <h1 className="font-serif font-bold text-white text-3xl">To-Do List</h1>
+      <div className="border-2 border-white rounded-lg w-2/3 h-2/3 p-4"></div>
+    </div>
+  );
 };
 
 export default App;
