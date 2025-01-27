77ef98253fecaaecb1f731a778d76208cad0ed63
Raphael <naduma.raphael4@gmail.com>
2025-01-27 20:46:41 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,19 @@ const App = () => {
     // bg-linear-to-bl from-indigo-950 via-gray-900 to-black
 
     <div className="absolute space-y-8 inset-0 p-4 w-full h-full bg-cover bg-no-repeat bg-fixed bg-center flex flex-col justify-center items-center bg-[url('https://usagif.com/wp-content/uploads/gifs/starfall-gif-46.gif')]">
-
-      <h1 className="font-serif font-bold text-white text-5xl mt-auto">To-Do List</h1>
+      <h1 className="font-serif font-bold text-white text-5xl mt-auto">
+        To-Do List
+      </h1>
 
       <TodoContainer>
         <TodoList></TodoList>
       </TodoContainer>
+      <p className="text-2xl text-white italic">
+        The checkbox for the "Mark as Done" feature isn't behaving as intended.
+      </p>
+      <p className="text-2xl text-white italic">
+        Editing is performed through a window prompt.
+      </p>
     </div>
   );
 };
