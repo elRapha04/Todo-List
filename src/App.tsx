56c1a6d4ab86520fde5de32e449598e2f6b5372d
Raphael <naduma.raphael4@gmail.com>
2025-01-27 08:52:19 +0800

    B4 TAILWIND v4.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div className="w-full h-screen flex justify-center items-center">App</div>;
+  return <div className="w-full h-screen flex justify-center items-center">before tail v4</div>;
 };
 
 export default App;
