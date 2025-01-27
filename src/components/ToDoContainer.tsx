7dbb609d318b530a4eac2dc8e77774372a17b71a
Raphael <naduma.raphael4@gmail.com>
2025-01-27 12:58:13 +0800

    format todo item

--- src/components/ToDoContainer.tsx
+++ src/components/ToDoContainer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const TodoContainer = ({children}) => {
   return (
-    <div className="w-2/3 h-2/3 border-2 border-white rounded-lg p-4 flex flex-col justify-center items-center bg-linear-to-bl from-transparent via-gray-200/20 to-white/20 backdrop-blur-sm">
+    <div className="w-2/3 h-2/3 border-2 border-white rounded-lg space-y-4 p-4 flex flex-col justify-center items-center bg-linear-to-bl from-transparent via-gray-200/20 to-white/20 backdrop-blur-sm">
       {children}
     </div>
   );
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Checkbox } from "./ui/checkbox";
+
 
 const TodoItem = () => {
-  return <div>TodoItem</div>;
+  return (
+  <div className="w-full h-1/5 border-2 border-lime-400 rounded-2xl p-2 flex justify-start space-x-10 items-center">
+    <Checkbox />
+    <p>Lorem Ipsum Dolor</p>
+  </div>
+  )
 };
 
 export default TodoItem;
