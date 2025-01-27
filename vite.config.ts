8a4f289123ad5388efc9ff330c51524f0a8f4263
Raphael <naduma.raphael4@gmail.com>
2025-01-27 22:35:44 +0800

    FINISHED DEPLOYMENT

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/todo-list",
+  base: "/todo-list/",
   plugins: [react()],
   resolve: {
     alias: {
