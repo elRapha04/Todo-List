e5fed2e7c179b868badcb4c58af06d130c9f00f4
Raphael <naduma.raphael4@gmail.com>
2025-01-27 16:00:18 +0800

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import TodoContainer from "./components/ToDoContainer";
 import TodoItem from "./components/TodoItem";
 import AddButton from "./components/AddButton";
+import TodoList from "./components/TodoList";
 
 const App = () => {
   return (
@@ -10,12 +11,9 @@ const App = () => {
     <div className="absolute inset-0 p-4 w-full h-full bg-cover bg-no-repeat flex flex-col justify-center items-center bg-[url('https://images.unsplash.com/photo-1736077722346-31ba59414728?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8')]">
       <h1 className="font-serif font-bold text-white text-3xl">To-Do List</h1>
 
-      <TodoContainer>
-        <TodoItem></TodoItem>
+      <TodoList>
         
-
-        <AddButton></AddButton>
-      </TodoContainer>
+      </TodoList>
     </div>
   );
 };
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+const TodoList = () => {
+
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  function handleInput(event){
+    
+  }
+
+  function addTask(event){
+
+  }
+
+  function deleteTask(event){
+
+  }
+
+  function moveUp(event){
+
+  }
+
+  function moveDown(event){
+
+  }
+
+
+  return (<></>);
+};
+
+export default TodoList;
