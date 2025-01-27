11c3d7b926a4eb21a4345d26a811b8153f06111a
Raphael <naduma.raphael4@gmail.com>
2025-01-27 13:49:03 +0800

    START FUNCS

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -5,7 +5,7 @@ import { MdDeleteOutline } from "react-icons/md";
 const TodoItem = () => {
   return (
     <div className="w-full h-1/5 border-2 border-lime-400 rounded-2xl p-2 flex justify-between space-x-10 items-center">
-      <Checkbox className="cursor-pointer hover:bg-black/10 active:bg-white" />
+      <Checkbox className="cursor-pointer hover:bg-black/10 active:bg-white"/>
       <p>Lorem Ipsum Dolor</p>
       <MdDeleteOutline className="size-6 rounded-sm cursor-pointer bg-red-500 hover:bg-red-700 hover:text-white active:text-red-700 active:bg-white" />
     </div>
