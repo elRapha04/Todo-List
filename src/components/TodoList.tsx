bdcce4caaa53b49a20a7e8466fafaf40f5a2dd86
Raphael <naduma.raphael4@gmail.com>
2025-01-27 16:22:37 +0800

    add items

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
+import { Checkbox } from "./ui/checkbox";
+import { MdDeleteOutline } from "react-icons/md";
 
 const TodoList = () => {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(["task 1", "task 2", "task 3"]);
   const [newTask, setNewTask] = useState("");
 
   function handleInput(event) {}
@@ -14,7 +16,22 @@ const TodoList = () => {
 
   function moveDown(index) {}
 
-  return <div></div>;
+  return (
+    <div className="w-full">
+      <ol>
+        {tasks.map((task, index) => (
+          <li
+            key={index}
+            className="w-full h-1/5 border-2 border-lime-400 rounded-2xl p-2 flex justify-between space-x-10 items-center"
+          >
+            <Checkbox className="cursor-pointer hover:bg-black/10 active:bg-white" />
+            <span>{task}</span>
+            <MdDeleteOutline className="size-6 rounded-sm cursor-pointer bg-red-500 text-black hover:bg-red-700 hover:text-white active:text-red-700 active:bg-white" />
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 };
 
 export default TodoList;
