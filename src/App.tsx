fae2a5e9b19430eeddc98d38b82d8319e6b4f398
Raphael <naduma.raphael4@gmail.com>
2025-01-27 12:36:40 +0800

    created components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Button } from "./components/ui/button";
+import TodoContainer from "./components/ToDoContainer";
+import TodoItem from "./components/TodoItem";
 
 const App = () => {
   return (
@@ -7,15 +9,18 @@ const App = () => {
 
     <div className="absolute inset-0 p-4 w-full h-full bg-cover bg-no-repeat flex flex-col justify-center items-center bg-[url('https://images.unsplash.com/photo-1736077722346-31ba59414728?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8')]">
       <h1 className="font-serif font-bold text-white text-3xl">To-Do List</h1>
-      <div className="w-2/3 h-2/3 border-2 border-white rounded-lg p-4 flex flex-col justify-center items-center bg-linear-to-bl from-transparent via-gray-200/20 to-white/20 backdrop-blur-sm">
-        <Button
-          variant={"outline"}
-          className="group hover:bg-transparent hover:border-3 transition-all duration-500"
-        >
-          <p className="">+</p>
-          <p className="hidden group-hover:inline">New Note</p>
-        </Button>
-      </div>
+
+      <TodoContainer>
+        <TodoItem>
+        </TodoItem>
+          <Button
+            variant={"outline"}
+            className="group hover:bg-transparent hover:border-3 transition-all duration-500"
+          >
+            <p className="">+</p>
+            <p className="hidden group-hover:inline">New Note</p>
+          </Button>
+      </TodoContainer>
     </div>
   );
 };
--- src/components/ToDoContainer.tsx
+++ src/components/ToDoContainer.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const ToDoContainer = () => {
-  return <div>ToDoContainer</div>;
+const TodoContainer = ({children}) => {
+  return (
+    <div className="w-2/3 h-2/3 border-2 border-white rounded-lg p-4 flex flex-col justify-center items-center bg-linear-to-bl from-transparent via-gray-200/20 to-white/20 backdrop-blur-sm">
+      {children}
+    </div>
+  );
 };
 
-export default ToDoContainer;
+export default TodoContainer;
