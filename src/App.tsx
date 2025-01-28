eb2e7613bfa207f648c9900834853066b97c4df5
Raphael <naduma.raphael4@gmail.com>
2025-01-28 10:18:33 +0800

    FINAL

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App = () => {
 
     
     <div className="absolute overflow-x-hidden space-y-8 inset-0 p-4 w-full h-full bg-cover bg-no-repeat bg-fixed bg-center flex flex-col justify-center items-center bg-[url('https://usagif.com/wp-content/uploads/gifs/starfall-gif-46.gif')]">
-      <h1 className="font-serif font-bold text-white text-3xl sm:text-5xl mt-auto">
+      <h1 className="font-serif font-bold text-white text-4xl sm:text-5xl mt-auto">
         To-Do List
       </h1>
 
--- src/components/ToDoContainer.tsx
+++ src/components/ToDoContainer.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 const ToDoContainer = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="mb-auto sm:w-4/5 h-4/5 sm:h-3/4 md:w-3/4 md:h-3/5 lg:w-3/5 xl:w-2/5 border-2 border-white rounded-xl space-y-4 p-4 flex flex-col items-center overflow-y-scroll scroll-smooth custom-scrollbar bg-linear-to-tr from-transparent via-gray-200/20 to-white/20 backdrop-blur-xs transition-all duration-500">
+    <div className="mb-auto w-9/10 sm:w-4/5 h-4/5 sm:h-3/4 md:w-3/4 md:h-3/5 lg:w-3/5 xl:w-2/5 border-2 border-white rounded-xl space-y-4 p-4 flex flex-col items-center overflow-y-scroll scroll-smooth custom-scrollbar bg-linear-to-tr from-transparent via-gray-200/20 to-white/20 backdrop-blur-xs transition-all duration-500">
       {children}
     </div>
   );
